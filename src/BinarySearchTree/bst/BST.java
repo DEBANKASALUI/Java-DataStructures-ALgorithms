972d38bbbc8d690614653e57273e80571baee058
deban <debankasalui@gmail.com>
2025-04-13 12:08:14 +0530

    Added Binary Search Tree

--- src/BinarySearchTree/bst/BST.java
+++ src/BinarySearchTree/bst/BST.java
@@ -27,10 +27,87 @@ public class BST {
 
     //    Delete Method
     private Node delete(Node root, int data) {
+        if (root == null) {
+            return null;
+        }
+        if (data < root.data) {
+            root.left = delete(root.left, data);
+        } else if (data > root.data) {
+            root.right = delete(root.right, data);
+        } else {
+//            Case 1: Node with no Children(Leaf Node)
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+//            Case 2: Node with 1 child
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+//            Case 3: Node with 2 Children
+            else {
+                Node successor = findMin(root.right);
+                root.data = successor.data;
+                root.right = delete(root.right, successor.data);
+            }
 
+        }
+        return root;
+    }
+
+    public Node findMin(Node root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
     }
 
     public void delete(int data) {
         root = this.delete(root, data);
+    }
+
+    //    Search Method
+    private boolean search(Node root, int data) {
+        if (root == null) {
+            return false;
+        }
+        if (root.data == data) {
+            return true;
+        }
+        return data < root.data ? search(root.left, data) : search(root.right, data);
+    }
+
+    public boolean search(int data) {
+        return search(root, data);
+    }
+
+    //    Traversal:
+//    Pre-Order(Depth-First-Search)Traversal:Root->Left->Right
+    public void preOrder(Node node) {
+        if (node == null)
+            return;
+        System.out.print(node.data + "\t");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    //    In-Order(Depth-First-Search)Traversal:Left->Root->Right
+    public void inOrder(Node node) {
+        if (node == null)
+            return;
+        inOrder(node.left);
+        System.out.print(node.data + "\t");
+        inOrder(node.right);
+    }
+
+
+    //    Post-Order(Depth-First-Search)Traversal:Left->Right->Root
+    public void postOrder(Node node) {
+        if (node == null)
+            return;
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.data + "\t");
     }
 }
--- src/BinarySearchTree/bst/BSTMain.java
+++ src/BinarySearchTree/bst/BSTMain.java
@@ -2,10 +2,48 @@ package BinarySearchTree.bst;
 
 public class BSTMain {
     public static void main(String[] args) {
-//        Insertion
-//        Deletion
-//        Search
-//        Traversal
 
+        BST tree = new BST();
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(70);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(80);
+
+//        Traversal
+        System.out.println("Inorder traversal:");
+        tree.inOrder(tree.root);
+
+        System.out.println();
+        System.out.println("PostOrder traversal:");
+        tree.postOrder(tree.root);
+
+        System.out.println();
+        System.out.println("PreOrder traversal:");
+        tree.preOrder(tree.root);
+
+//        Deletion
+        System.out.println();
+        System.out.println("Deleting 80:");
+        tree.delete(80);
+        tree.inOrder(tree.root);
+
+//        Insertion
+        System.out.println();
+        System.out.println("Inserting 100:");
+        tree.insert(100);
+        tree.inOrder(tree.root);
+
+//      Search
+        System.out.println();
+        System.out.println("Searching 100:");
+        System.out.println("Found 100: "+tree.search(100));
+//        boolean search = tree.search(100);
+//        if(search){
+//            System.out.println("Value present in BST");
+//        }else{
+//            System.out.println("Value not present in BST");
+//        }
     }
 }
